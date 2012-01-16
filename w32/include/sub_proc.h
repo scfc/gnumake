046e53dbbd38c10e7c5346cf3717357deef0bf7a
psmith <psmith>
2012-01-16 02:29:20 +0000

    Update copyright notices.

--- w32/include/sub_proc.h
+++ w32/include/sub_proc.h
@@ -1,6 +1,6 @@
 /* Definitions for Windows process invocation.
-Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-2006, 2007, 2008, 2009, 2010 Free Software Foundation, Inc.
+Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+2007, 2008, 2009, 2010, 2011, 2012 Free Software Foundation, Inc.
 This file is part of GNU Make.
 
 GNU Make is free software; you can redistribute it and/or modify it under the
@@ -21,11 +21,11 @@ this program.  If not, see <http://www.gnu.org/licenses/>.  */
 /*
  * Component Name:
  *
- * $Date: 2011/11/15 21:12:54 $
+ * $Date: 2012/01/16 02:29:26 $
  *
  * $Source: /sources/make/make/w32/include/sub_proc.h,v $
  *
- * $Id: sub_proc.h,v 1.14 2011/11/15 21:12:54 psmith Exp $
+ * $Id: sub_proc.h,v 1.15 2012/01/16 02:29:26 psmith Exp $
  */
 
 #define EXTERN_DECL(entry, args) extern entry args
